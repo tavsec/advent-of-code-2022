6ef40ebb72c8c93c2ed71aebcfb83882366fa34c
Timotej Avsec <avsectimotej1@gmail.com>
2022-12-04 09:19:10 +0100

    Add day-4, part 1

---
+++ day-4/part1.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func splitRange(id string) (int, int) {
+	splited := strings.Split(id, "-")
+	parsed1, _ := strconv.ParseInt(splited[0], 0, 32)
+	parsed2, _ := strconv.ParseInt(splited[1], 0, 32)
+	return int(parsed1), int(parsed2)
+}
+
+func fullyContain(id1 ID, id2 ID) bool {
+	return (id1.Start <= id2.Start && id1.End >= id2.End) || (id2.Start <= id1.Start && id2.End >= id1.End)
+}
+
+type ID struct {
+	Start int
+	End   int
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic("Couldn't open input.txt")
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	counter := 0
+	for fileScanner.Scan() {
+		val := fileScanner.Text()
+		ids := strings.Split(val, ",")
+
+		start, end := splitRange(ids[0])
+		id1 := ID{Start: start, End: end}
+		start, end = splitRange(ids[1])
+		id2 := ID{Start: start, End: end}
+		if fullyContain(id1, id2) {
+			counter++
+		}
+	}
+
+	fmt.Println("Number of IDs that contains each other: " + fmt.Sprint(counter))
+}
