f18057b6f8d21dd04352107db4988e80b6beddc4
Timotej Avsec <avsectimotej1@gmail.com>
2022-12-10 14:13:36 +0100

    Add day-9 readme

---
+++ day-9/part1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic("Couldn't open input.txt")
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		val := fileScanner.Text()
+		fmt.Println(val)
+	}
+}
